14fc3ca5ca1251f03c3cf6e075878f10a2d65c5d
Taras Boikov <tarasgowest@gmail.com>
2021-10-06 23:25:15 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+startBtn.addEventListener('click');
+stopBtn.addEventListener('click');
+
+console.log(startBtn);
