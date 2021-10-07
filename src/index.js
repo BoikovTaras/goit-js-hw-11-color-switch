6c2b864ff970fb4795d196e7fd0f0ae91b9d9494
Taras Boikov <tarasgowest@gmail.com>
2021-10-07 21:51:43 +0300

    перезаписывает стартовые файлы

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const startBtn = document.querySelector('[data-action="start"]');
-const stopBtn = document.querySelector('[data-action="stop"]');
-startBtn.addEventListener('click');
-stopBtn.addEventListener('click');
-
-console.log(startBtn);
